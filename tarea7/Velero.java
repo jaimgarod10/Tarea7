de2af15dee0a4a396000bed132b393ce9d614f3e
Jaime <56158062+jaimgarod10@users.noreply.github.com>
2020-04-28 10:31:10 +0200

    tarea7

---
+++ tarea7/Velero.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea7;
+
+/**
+ *
+ * @author jaimg
+ */
+public class Velero extends Barco{
+    
+    private int numeroMastiles;
+    private float precio;
+
+    public Velero() {
+    }
+    
+    public Velero(int numeroMastiles, float eslora, int dias, String matricula) {
+        super(eslora, dias, matricula);
+        this.numeroMastiles = numeroMastiles;
+    }
+
+    public int getNumeroMastiles() {
+        return numeroMastiles;
+    }
+
+    public void setNumeroMastiles(int numeroMastiles) {
+        this.numeroMastiles = numeroMastiles;
+    }
+
+    @Override
+    public void calcularPrecio() {
+        precio = 10*super.getEslora()*super.getDias()+8*numeroMastiles;
+    }
+
+    @Override
+    public String toString() {
+        return "Velero" + " precio: " + precio;
+    }
+
+}
